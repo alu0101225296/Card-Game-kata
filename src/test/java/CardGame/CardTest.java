04cf1469f6cfd69ccb69789ea47706e57753285b
alu0101225296 <alu0101225296@ull.edu.es>
2022-03-30 10:16:19 +0100

    setup

---
+++ src/test/java/CardGame/CardTest.java
@@ -0,0 +1,11 @@
+package CardGame;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardTest {
+    @Test
+    void create_valid_card() {
+
+    }
+}
