d22b02921ff25788904f1d01e6f1aaa753ba0d9d
alu0101225296 <alu0101225296@ull.edu.es>
2022-03-30 10:30:35 +0100

    create card fail

--- src/test/java/CardGame/CardTest.java
+++ src/test/java/CardGame/CardTest.java
@@ -1,11 +1,12 @@
 package CardGame;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CardTest {
     @Test
     void create_valid_card() {
-
+       Card card = new Card("2");
+       assertNotNull(card);
     }
 }
